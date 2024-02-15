667baf78459621fc748f478b5a04b79613f39022
Angellah khumula <bsc-com-22-20@unima.ac.mw>
2024-02-15 19:46:49 +0200

    added images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,9 @@ import React from 'react'
 function Navbar() {
   return (
     <div className="navbar"> 
+    <div className="leftside"></div>
+    <div className="rightside"></div>
+
 
     </div>
   )
