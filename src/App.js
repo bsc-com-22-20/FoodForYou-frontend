3d26514eba58c61bc84a51b531a199c0af50bd1f
Angellah khumula <bsc-com-22-20@unima.ac.mw>
2024-02-15 22:02:06 +0200

    added a routing feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-
+import Home from './pages/Home';
+import {BrowserRouter as Router, Routes, Route  } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+    <Router>
       <Navbar/>
+      {/* <Switch> */}
+      <Routes>
+        <Route path="/"  element={<Home/>} />
+      {/* </Switch> */}
+      </Routes>
+    </Router>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Home() {
+  return (
+  <div>
+    Hello customers
+  </div>
+  );
+    
+  
+}
+
+export default Home
