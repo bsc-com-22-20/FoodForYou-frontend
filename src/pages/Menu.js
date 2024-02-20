d700303f5dfdfd20f55df797e5d9c17823c6925e
Angellah khumula <bsc-com-22-20@unima.ac.mw>
2024-02-20 18:19:11 +0200

    modified menuItem.js

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -18,8 +18,31 @@
 
 // export default Menu
 
+// import React from 'react';
+// import { MenuList } from '../helpers/MenuList';
+// import MenuItem from '../components/MenuItem';
+// function Menu() {
+//   return (
+//     <div className="menu">
+//       <h1 className="menuTitle">Our Menu</h1>
+//       <div className="menuList">
+//         {MenuList.map((menuItem, key) => (
+//           <div key={key}>{MenuItem 
+//             image={menuItem.image}
+//             name={menuItem.name}
+//             price={menuItem.price}
+//           }</div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Menu;
+
 import React from 'react';
 import { MenuList } from '../helpers/MenuList';
+import MenuItem from '../components/MenuItem';
 
 function Menu() {
   return (
@@ -27,7 +50,13 @@ function Menu() {
       <h1 className="menuTitle">Our Menu</h1>
       <div className="menuList">
         {MenuList.map((menuItem, key) => (
-          <div key={key}>{menuItem.name}</div>
+          <div key={key}>
+            <MenuItem
+              image={menuItem.image}
+              name={menuItem.name}
+              price={menuItem.price}
+            />
+          </div>
         ))}
       </div>
     </div>
