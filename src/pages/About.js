544848e8ed12cc176c9cf08c33e4e0c2d471272c
Angellah khumula <bsc-com-22-20@unima.ac.mw>
2024-02-20 19:11:17 +0200

    modified about.css

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import multiplePizzas from "../assets/multiplePizzas.jpeg";
-
+import "../styles/About.css"
 function About() {
   return (
     <div className="about">
        <div className="aboutTop"
-       style={{backgroundImage: `url(${multiplePizzas})` }} > djeweiuqeeqknlw </div>
+       style={{backgroundImage: `url(${multiplePizzas})` }} ></div>
        <div className="aboutBottom">
         <h1> ABOUT US</h1>
         <p> our restuarant serves several kinds of food and we are located in Lilongwe</p>
